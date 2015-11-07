a8fa9bdc5212847ec9e11f52a5d0c3f8911c59ec
lengstrom <lengstrom@users.noreply.github.com>
2015-11-06 23:49:56 -0500

    added transfer files

--- track.py
+++ track.py
@@ -2,27 +2,32 @@ import numpy as np
 import pdb
 import cv2
 
-face_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv/2.4.12/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv/2.4.12/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+#face_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv/2.4.12/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)#'./test_set.mov') # 0 for webcam
 n = 0
 while(True):
     n+=1
     # Capture frame-by-frame
     ret, frame = cap.read()
-    img = frame[10:710, 250:1030]
+#    img = frame[10:710, 250:1030]
+    img = frame
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(220,250))
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2, minSize=(250,250))
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+        cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),2)
+        
+        
+        #roi_gray = gray[y:y+h, x:x+w]
+        #roi_color = img[y:y+h, x:x+w]
 
     # Our operations on the frame come here
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-   
+    
     # Display the resulting frame
-    cv2.imshow('frame',img)
+    cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
