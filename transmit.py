418c6ebafc94641916f5fe265fdc5f02ad31b129
lengstrom <lengstrom@users.noreply.github.com>
2015-11-07 14:15:06 -0500

    made transmit real time

--- transmit.py
+++ transmit.py
@@ -1,24 +1,27 @@
 import numpy as np
 import cv2, pdb, requests, time
 
-cap = cv2.VideoCapture('test_set.mov')
+cap = cv2.VideoCapture(0)
 n = 0.
 avg = 0.
 
-print "preprocessing..."
-go = True
-frames = []
-while go:
-    go, frame = cap.read()
-    if go:
-        frame = frame[40:680, 320:960]
-        _, enc = cv2.imencode('.png', frame)
-        s = enc.tostring()
-        frames.append((s, frame))
+# print "preprocessing..."
+# go = True
+# frames = []
+# while go:
+#     go, frame = cap.read()
+#     if go:
+#         frame = frame[40:680, 320:960]
+#         _, enc = cv2.imencode('.png', frame)
+#         s = enc.tostring()
+#         frames.append((s, frame))
 headers = {'Content-Type': 'application/octet-stream'}
 
-for s, frame in frames:
-    n += 1
+while True:
+    go, frame = cap.read()
+    frame = frame[40:680, 320:960]
+    _, enc = cv2.imencode('.png', frame)
+    s = enc.tostring()
     r__ = requests.post("http://127.0.0.1:5000/", data=s, headers=headers)#files = {'i':s})
     if r__.text != '_':
         x, y, w, h = map(lambda x: int(x), r__.text.split(','))
